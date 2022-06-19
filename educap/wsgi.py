87815abbf5d7c87c1fbb2e6b87a7c3b14a561198
nageshmudgal <71936065+nageshmudgal@users.noreply.github.com>
2022-06-19 16:35:53 +0530

    new2

--- educap/wsgi.py
+++ educap/wsgi.py
@@ -7,10 +7,23 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
+# import os
+#
+# from django.core.wsgi import get_wsgi_application
+#
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'educap.settings')
+#
+# application = get_wsgi_application()
+
+
 import os
-
-from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'educap.settings')
 
+
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
